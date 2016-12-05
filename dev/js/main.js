2c87c625122feff385b88c0eff479b75579e043b
Osman Ramazanov <osman.ramazanov@gmail.com>
2016-12-05 18:32:14 +0200

    finished popups

--- dev/js/main.js
+++ dev/js/main.js
@@ -42,29 +42,38 @@ function showHideLangList() {
 function popupToModal(toModal) {
     if (toModal) {
         $('.popup').each(function () {
-            $(this).removeClass('popup-active');
-            if($(this).has('.popup-bottom')){
+            if ($(this).hasClass('popup-bottom')) {
                 $(this).addClass('bottom-sheet');
             }
-            if($(this).has('.popup-fixed-footer')){
+            if ($(this).hasClass('popup-fixed-footer')) {
                 $(this).addClass('modal-fixed-footer');
             }
 
+            $(this).removeClass('popup-active');
         });
     } else {
         $('.popup').each(function () {
-            $(this).addClass('popup-active');
-            if($(this).has('.popup-bottom')){
+            if ($(this).hasClass('popup-bottom')) {
                 $(this).removeClass('bottom-sheet');
             }
-            if($(this).has('.popup-fixed-footer')){
+            if ($(this).hasClass('popup-fixed-footer')) {
                 $(this).removeClass('modal-fixed-footer');
             }
+
+            $(this).addClass('popup-active');
         });
     }
 }
 $(document).ready(function () {
-    $(".button-collapse").sideNav();
+    $('.button-collapse').sideNav({
+        menuWidth: 320, // Default is 240
+        edge: 'left', // Choose the horizontal origin
+        draggable: true
+    });
+
+    $('.side-nav, .section-one').fullHeight();
+
+
     $('#lang-but').dropdown();
     $('#lang-list li').each(function () {
         langListW += $(this).width();
@@ -86,6 +95,15 @@ $(document).ready(function () {
         $('select').material_select();
     });
 
-    $('.modal-trigger').leanModal();
+    $('.modal-trigger').leanModal({
+        starting_top: 0,
+    });
+
+    $('.side-nav-modal').click(function (e) {
+        var href = $(this).attr('href');
+        // $(href).leanModal('open');
+
+    });
+
 
 });
